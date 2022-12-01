24e47774bfe28dbbcf9c0ebfac703ace757c569f
Igor Alexey <igor-alexey@hotmail.com>
2022-12-01 02:36:04 -0300

    glacial integration

--- core/init.cpp
+++ core/init.cpp
@@ -6,37 +6,41 @@
 namespace Digiplane {
 
     DPAPI int Init(initInfo* config) {
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window;
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+        /* Initialize the library */
+        if(!glfwInit()) {
+            std::cerr << "Failed to initialize GLFW code " << glfwGetError(nullptr) << "\n";
+            return -1;
+        }
+        bgfx::init();
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(config->width, config->height, config->title.c_str(), NULL, NULL);
-    if (!window)
-    {
+        /* Create a windowed mode window */
+        _DPCORE::window = glfwCreateWindow(config->width, config->height, config->title.c_str(), NULL, NULL);
+        if (!_DPCORE::window) {
+            glfwTerminate();
+            return -1;
+        }
+
+        /*Define BGFX platformData object */
+        _DPCORE::platformData.nwh = glfwGetWindowCrossPlatform(_DPCORE::window); // function defined in ./pch.hpp
+
+        /* Make the window's context current */
+        glfwMakeContextCurrent(_DPCORE::window);
+
+        /* Loop until the user closes the window */
+        while (!glfwWindowShouldClose(_DPCORE::window))
+        {
+            /* Render here */
+
+            /* Swap front and back buffers */
+            glfwSwapBuffers(_DPCORE::window);
+
+            /* Poll for and process events */
+            glfwPollEvents();
+        }
+
         glfwTerminate();
-        return -1;
-    }
-
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
-    return 0;
+        return 0;
     }
 } // DigiPlane
--- core/internal.hpp
+++ core/internal.hpp
@@ -1,10 +1,13 @@
 #ifndef INTERNAL_HPP
 #define INTERNAL_HPP
 
+// pch.hpp comes from init.cpp, no need for redundant include, although some IDEs may complain, but not compilers.
+
 // used to contain all mutable state variables
 namespace _DPCORE {
     bool initialized;
-
+    GLFWwindow* window;
+    bgfx::PlatformData platformData;
     // context
 
 
