d3e45ec52e24918e08761a37ae0fb0cf17217019
Igor Alexey <igor-alexey@hotmail.com>
2022-12-31 03:22:56 -0300

    utility FPS

--- Runtime/Main.cpp
+++ Runtime/Main.cpp
@@ -13,5 +13,15 @@ void RuntimeApp::init() {
 }
 
 void RuntimeApp::update(float deltaTime) {
+    static int fpsCount = 0;
+    fpsCount++;
 
+    static double frameDelta = 0.0;
+    frameDelta += deltaTime;
+
+    if (frameDelta > 1.0) {
+        std::cout << "FPS: " << fpsCount << std::endl;
+        fpsCount = 0;
+        frameDelta = 0.0;
+    }
 }
